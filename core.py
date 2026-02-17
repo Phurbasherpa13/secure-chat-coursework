8839950755092ea50af8c9392aff72506ef31752
phurbasherpa13 <13phurbasherpa@gmail.com>
2026-02-17 12:41:27 +0545

    feat: updated core script with the init function

--- core.py
+++ core.py
@@ -3,6 +3,12 @@ import threading
 import websockets
 from security import SecurityEngine
 
-
 class ChatCore:
-    pass
+    def __init__(self, log_callback, connected_callback, disconnected_callback):
+        self.log = log_callback
+        self.on_connected = connected_callback
+        self.on_disconnected = disconnected_callback
+        self.websocket = None
+        self.loop = None
+        self.security = None
+        self.running = False
