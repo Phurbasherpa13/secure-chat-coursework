6181eec02938609b70ed82c63eaadee4d3995343
phurbasherpa13 <13phurbasherpa@gmail.com>
2026-02-17 12:36:37 +0545

    feat: updated GUI script with the modules and libraries

--- gui.py
+++ gui.py
@@ -1,2 +1,10 @@
-def gui():
+# Imported Modules
+import tkinter as tk
+from tkinter import ttk, scrolledtext, messagebox
+import asyncio
+from core import ChatCore
+from utils import save_to_file, load_history
+
+# This class handles the graphical interface of the application
+class SecureChatApp:
     pass
