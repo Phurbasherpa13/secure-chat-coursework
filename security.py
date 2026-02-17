86a32058fd5222eedfdb9ab321443f60d3f8970a
phurbasherpa13 <13phurbasherpa@gmail.com>
2026-02-17 12:31:44 +0545

    feat: add security.py to handle encryption and decryption of the message

---
+++ security.py
@@ -0,0 +1,24 @@
+
+import hashlib
+import base64
+from cryptography.fernet import Fernet, InvalidToken
+
+# This class handles Encryption/Decryption using Fernet (AES-128).
+class SecurityEngine:
+
+    def __init__(self, password: str):
+        self.key = self._generate_key_from_password(password)
+        self.cipher = Fernet(self.key)
+
+    def _generate_key_from_password(self, password: str):
+        digest = hashlib.sha256(password.encode()).digest()
+        return base64.urlsafe_b64encode(digest)
+
+    def encrypt_message(self, message: str) -> bytes:
+        return self.cipher.encrypt(message.encode("utf-8"))
+
+    def decrypt_message(self, encrypted_bytes: bytes) -> str:
+        try:
+            return self.cipher.decrypt(encrypted_bytes).decode("utf-8")
+        except InvalidToken:
+            return "[Error: Decryption Failed]"
