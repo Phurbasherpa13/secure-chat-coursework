adae1ccdf6664007713902359ed767f086fca0b7
phurbasherpa13 <13phurbasherpa@gmail.com>
2026-02-17 12:40:40 +0545

    feat: added a functional logic to initialize the applicaiton header

--- gui.py
+++ gui.py
@@ -7,4 +7,24 @@ from utils import save_to_file, load_history
 
 # This class handles the graphical interface of the application
 class SecureChatApp:
-    pass
+    
+    # This is the intializer function to intialize application heading information
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Secure WebSocket Chat")
+        self.root.geometry("600x700")
+        self.root.configure(bg="#1e1e1e")
+
+        self.log_file = "chat_history.txt"
+
+        self.core = ChatCore(
+            log_callback=self.log_to_chat,
+            connected_callback=self.on_connected,
+            disconnected_callback=self.on_disconnected
+        )
+
+        self.create_widgets()
+        self.core.start_thread()
+        self.load_history()
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
